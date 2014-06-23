4289bf21a9f39e6a079542ab871ecf153a65726a
Austin Jackson mac@ruthlessphysics.com <mac@ruthlessphysics.com>
2014-06-22 22:57:58 -0500

    Added pDraw and pTicTacToe to index.html, and made the current theme preference save in cookies

--- js/themes.js
+++ js/themes.js
@@ -1,6 +1,46 @@
 //I wonder when I'll need to use JavaScript
 
-var theme = "light";
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toGMTString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+
+var theme = getCookie("theme");
+document.getElementById("currentTheme").href="css/roboto"+theme+".css";
+if(theme == "light")
+{
+    document.getElementById("swapper").innerHTML="Switch to Dark mode"; 
+    alert("button light");
+}
+else if(theme == "dark")
+{
+    document.getElementById("swapper").innerHTML="Switch to Light mode"; 
+    alert("button dark");
+}
+else
+{
+    document.getElementById("swapper").innerHTML="???";
+}
+
+if(theme != "light" || theme != "dark")
+{
+    theme = "light";
+    setCookie("theme",theme,"9999999");
+}
 
 function setTheme(inputTheme)
 {
@@ -18,16 +58,22 @@ function switchTheme()
     {
         theme = "dark";
         document.getElementById("currentTheme").href="css/robotodark.css"; 
+        document.getElementById("swapper").innerHTML="Switch to Light mode"; 
+        setCookie("theme",theme,"9999999");
     }
     else if(theme == "dark")
     {
         theme = "light";
         document.getElementById("currentTheme").href="css/robotolight.css";
+        document.getElementById("swapper").innerHTML="Switch to Dark mode";
+        setCookie("theme",theme,"9999999");
     }
     else
     {
         theme = "light";
         document.getElementById("currentTheme").href="css/robotolight.css";
+        document.getElementById("swapper").innerHTML="Switch to Dark mode"; 
+        setCookie("theme",theme,"9999999");
     }
     window.reload();
 }
