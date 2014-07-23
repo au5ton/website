b7add527448b44d8b542eb5204e7f652e151def4
Austin Jackson themacphage@gmail.com <themacphage@gmail.com>
2014-07-22 23:51:44 -0500

    Major site update
    Replaced applicable PNG icons with FontIcons and added new messages to
    motd.js

--- js/motd.js
+++ js/motd.js
@@ -8,8 +8,17 @@ m[0] = "JavaScript functionality!";
 m[1] = "CSS riddled!";
 m[2] = "Triangles!";
 m[3] = "Absolutely no WebGL!";
-m[4] = "&lt;iframe&gt; utilizing!";
-m[5] = "<button onclick=alert(\"I_like_buttons._c:\")>Button featured!</button>";
+m[4] = "<button onclick=alert(\"I_like_buttons._c:\")>Button featured!</button>";
+m[5] = "Also checkout Srchub.org! They're awesome!";
+m[6] = "Now on <span class=\"mega-octicon octicon-logo-github\" style=\"font-size: 40px;\"></span>!";
+m[7] = "Uses Octocons!";
+m[8] = "Uses Socialico!";
+m[9] = "Say hi to teen programmers!";
+m[10] = "Thank @qrohlf for the background!";
+m[11] = "Say hi to @zorcan1!";
+m[12] = "Say hi to @Jebs_Helper!";
+m[13] = "Say hi to @JavaFTW!";
+
 
 function motd_apply()
 {
