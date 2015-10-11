a4844b4e0530d4b995c6b7502595ba72763ca940
Austin Jackson <au5ton@icloud.com>
2015-10-11 00:19:41 -0500

    Fixed some comments

--- js-dev/priority/main.js
+++ js-dev/priority/main.js
@@ -1,6 +1,16 @@
 
-//Geopattern pattern
+/*
 
+Main.js
+Client-code for austinj.net
+
+If you're reading this from libraries.js, lol look at all the compressed code above, huh?
+This is so we can use the async HTML attribute to make JavaScript loading non-blocking 😎
+
+*/
+
+
+//Geopattern pattern
 try {
     var pattern = GeoPattern.generate(new Date());
     document.getElementById('canvas').style.background = pattern.toDataUrl();
@@ -40,32 +50,31 @@ GitHubActivity.feed({
 
 
 //Most used languages
-
-//data from austinj-stats
-var data = {"Arduino":{"color":"#bd79d1","count":2},"HTML":{"color":"#e44b23","count":10},"Java":{"color":"#b07219","count":9},"JavaScript":{"color":"#f1e05a","count":9}};
 var keys = []; //keys to the Dictionary (object)
 var graph_data = [];
+var ctx;
+var myDoughnutChart;
+jQuery.get('https://austinjnet-stats.herokuapp.com/api/mostusedlanguages?include_colors=true',function(data){
+    /*
+    Gets the names of the keys and saves them in `keys` so that we can access
+    `data` with bracket notation (like Swift dictionaries)
+    */
+    for(prop in data) {
+        keys.push(prop);
+    }
 
-/*
-Gets the names of the keys and saves them in keys so that we can access
-`data` with bracket notation (like Swift dictionaries)
-*/
-for(prop in data) {
-    keys.push(prop);
-}
-
-for(var i = 0; i < keys.length; i++) {
-    graph_data.push({
-        value: data[keys[i]]['count'],
-        color: data[keys[i]]['color'],
-        label: keys[i]
-    });
-}
-
-// Get the context of the canvas element we want to select
-var ctx = document.getElementById('myChart').getContext('2d');
-// And for a doughnut chart
-var myDoughnutChart = new Chart(ctx).Pie(graph_data);
+    for(var i = 0; i < keys.length; i++) {
+        graph_data.push({
+            value: data[keys[i]]['count'],
+            color: data[keys[i]]['color'],
+            label: keys[i]
+        });
+    }
+    // Get the context of the canvas element we want to select
+    ctx = document.getElementById('myChart').getContext('2d')
+    // And for a doughnut chart
+    myDoughnutChart = new Chart(ctx).Pie(graph_data)
+});
 
 
 //Auto Copyright
